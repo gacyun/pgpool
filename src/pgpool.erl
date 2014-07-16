8548d9440d8f2e405dc5bc23c704da0a3f1638c1
Sonic Gao <sonic.gaochao@gmail.com>
2014-07-16 09:55:01 +0800

    improve & cleanup

---
+++ src/pgpool.erl
@@ -0,0 +1,108 @@
+-module(pgpool).
+-behaviour(application).
+-behaviour(supervisor).
+
+-export([start/0, stop/0, start/2, stop/1, init/1, squery/2, equery/3, equery/4, status/1]).
+
+
+% -define(DEBUG_QUERY_LEVEL, io).
+% -define(DEBUG_QUERY_LEVEL, error).
+
+-ifdef(DEBUG_QUERY_LEVEL).
+	-define(DEBUG_QUERY_NOW, DEBUG_QUERY_NOW = erlang:now()).
+	-define(DEBUG_QUERY(Format, Args),
+		case ?DEBUG_QUERY_LEVEL of
+			io ->
+				io:format("[PGPOOL]:~-2b [~8b] " ++ Format ++ "~n",
+					[?LINE, timer:now_diff(erlang:now(), DEBUG_QUERY_NOW) | Args]);
+			_ ->
+				log4erl:log(?DEBUG_QUERY_LEVEL, "[PGPOOL_DEBUG]:~b [~b] " ++ Format,
+				[?LINE, timer:now_diff(erlang:now(), DEBUG_QUERY_NOW) | Args])
+		end).
+-else.
+	-define(DEBUG_QUERY_NOW, ok).
+	-define(DEBUG_QUERY(Format, Args), ok).
+-endif.
+
+
+start() -> application:start(?MODULE).
+stop()	-> application:stop(?MODULE).
+
+start(_Type, _Args) ->
+	supervisor:start_link({local, pgpool_sup}, ?MODULE, []).
+stop(_State) -> ok.
+
+init([]) ->
+	{ok, Pools} = application:get_env(pgpool, pools),
+	Fun = fun
+		({PoolName, Size, DBUrl}) ->
+			Args = [{name, {local, PoolName}},
+				{worker_module, pgpool_worker},
+				{size, Size},
+				{db_url, DBUrl}],
+				{PoolName, {poolboy, start_link, [Args]},
+					permanent, 5000, worker, [poolboy]};
+		({PoolName, Size, Overflow, DBUrl}) ->
+			Args = [{name, {local, PoolName}},
+				{worker_module, pgpool_worker},
+				{size, Size},
+				{max_overflow, Overflow},
+				{db_url, DBUrl}],
+			{PoolName, {poolboy, start_link, [Args]},
+					permanent, 5000, worker, [poolboy]}
+	end,
+	PoolSpecs = lists:map(Fun, Pools),
+	{ok, {{one_for_one, 10000, 10}, PoolSpecs}}.
+
+squery(PoolName, Sql) ->
+	Worker = poolboy:checkout(PoolName),
+	try
+		R = gen_server:call(Worker, {squery, Sql}),
+		ok = poolboy:checkin(PoolName, Worker),
+		R
+	catch
+		exit:{timeout, {gen_server, call, [_Pid, {squery, SQL = <<"INSERT", _/binary>>}, _Timeout]}} ->
+			io:format("INSERT too slow!! ~p~n", [SQL]),
+			gen_server:cast(Worker, stop),
+			{error, execution_timeout};
+		exit:{timeout, {gen_server, call, [Pid, {squery, _SQL = <<"SELECT", _/binary>>}, _Timeout]}} ->
+			exit(Pid, kill),
+			{error, execution_timeout};
+		Exit:Reason ->
+			exit(Worker, kill),
+			{Exit, Reason}
+	end.
+
+equery(PoolName, Stmt, Params) ->
+	equery(PoolName, Stmt, Params, 5000).
+
+equery(PoolName, Stmt, Params, Timeout) ->
+	?DEBUG_QUERY_NOW,
+	?DEBUG_QUERY("B ~p ~p", [self(), Stmt]), % before checkout
+	Worker = poolboy:checkout(PoolName),
+	?DEBUG_QUERY("O ~p ~p ~p", [Worker, Stmt, Params]), % after checkout
+	try
+		R = gen_server:call(Worker, {equery, Stmt, Params}, Timeout),
+		ok = poolboy:checkin(PoolName, Worker),
+		?DEBUG_QUERY("I ~p ~p", [Worker, Stmt]), % after checkin
+		R
+	catch
+		exit:{timeout, {gen_server, call, [_Pid, {equery, SQL = <<"INSERT", _/binary>>, _Args}, _Timeout]}} ->
+			io:format("INSERT too slow!! ~p~n", [SQL]),
+			gen_server:cast(Worker, stop),
+			{error, execution_timeout};
+		exit:{timeout, {gen_server, call, [Pid, {equery, _SQL = <<"SELECT", _/binary>>, _Args}, _Timeout]}} ->
+			exit(Pid, kill),
+			{error, execution_timeout};
+		Exit:Reason ->
+			exit(Worker, kill),
+			{Exit, Reason}
+	end.
+
+status(PoolName) ->
+	case whereis(PoolName) of
+		undefined ->
+			{none, not_found};
+		_ ->
+			poolboy:status(PoolName)
+	end.
---
+++ src/pgpool_worker.erl
@@ -0,0 +1,45 @@
+-module(pgpool_worker).
+-behaviour(gen_server).
+
+-export([start_link/1, stop/0, init/1, handle_call/3, handle_cast/2,
+	handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {conn}).
+
+start_link(Args) -> gen_server:start_link(?MODULE, Args, []).
+stop() -> gen_server:cast(?MODULE, stop).
+
+init(Args) ->
+	% process_flag(trap_exit, true),
+	DB_URL = proplists:get_value(db_url, Args),
+	{pg, Host, Username, Password, Opts} = pgpool_utils:parse_db_url(DB_URL),
+	case pgsql:connect(Host, Username, Password, Opts) of
+		{ok, Conn} ->
+			{ok, #state{conn = Conn}};
+		{error, Reason} ->
+			{stop, {error, Reason}}
+	end.
+
+handle_call({squery, Sql}, _From, #state{conn=Conn}=State) ->
+	{reply, pgsql:squery(Conn, Sql), State};
+handle_call({equery, Stmt, Params}, _From, #state{conn=Conn}=State) ->
+	{reply, pgsql:equery(Conn, Stmt, Params), State};
+handle_call(_Request, _From, State) ->
+	{reply, ok, State}.
+
+handle_cast(stop, State) ->
+	{stop, shutdown, State};
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+handle_info({'EXIT', _, _}, State) ->
+	{stop, shutdown, State};
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+terminate(_Reason, #state{conn=Conn}) ->
+	pgsql:close(Conn),
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
