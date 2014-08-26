759aa683954c4f3d9c30a3b15386b3016212e5c6
Sonic Gao <sonic.gaochao@gmail.com>
2014-08-26 17:49:32 +0800

    support get config from system env

--- src/pgpool_utils.erl
+++ src/pgpool_utils.erl
@@ -9,10 +9,20 @@ parse_db_url(Url) ->
         [] -> [];
         [Options1] -> parse_db_options(string:tokens(Options1, "&"), [])
     end,
-    {list_to_atom(Schema), Host, User, Pass, [{database, DB}|Opts]}.
+    {list_to_atom(Schema), env_filter(Host), env_filter(User), env_filter(Pass), [{database, env_filter(DB)}|Opts]}.
 
 
 % private
+env_filter(Param) ->
+    case re:run(Param, "{{\s{0,}(.*[^\s])\s{0,}}}", [{capture, [1], list}]) of
+        nomatch -> Param;
+        {match,[EnvKey]} ->
+            case os:getenv(string:to_upper(EnvKey)) of
+                false -> "undefined";
+                Value -> Value
+            end
+    end.
+
 parse_db_options([], Result) -> Result;
 parse_db_options([Option|Options], Result) ->
     case string:tokens(Option, "=") of
